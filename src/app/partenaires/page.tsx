d8c1455b44cd78e62259cb022509f48065e6eb81
Loric <loric.bondon1@etu.univ-lorraine.fr>
2025-09-28 23:36:55 +0200

    Add descriptive text for partners sections
    Added explanatory paragraphs for institutionnels and fédéraux partners on the Partenaires page to provide more context and acknowledge their support. This improves clarity and recognition for key contributors to the club.

--- src/app/partenaires/page.tsx
+++ src/app/partenaires/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import PartenaireSection from "@/components/partenaires/PartnersSection";
 import SupportSection from "@/components/partenaires/SupportSection";
-import { partenairesInstitutionnels, partenairesFederaux, sponsors} from "@/data/partners";
+import {
+  partenairesInstitutionnels,
+  partenairesFederaux,
+  sponsors,
+} from "@/data/partners";
 
 export default function PartenairesPage() {
- return (
+  return (
     <section className="py-12 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-8">
@@ -15,7 +19,31 @@ export default function PartenairesPage() {
         </div>
 
         <PartenaireSection title="Institutionnels" partenaires={partenairesInstitutionnels} />
+        <div className="mb-8">
+          <p className="text-foreground/80 mb-12">
+            Le Bouzonville Handball tient à remercier chaleureusement ses
+            partenaires institutionnels – la Ville de Bouzonville, la Communauté
+            de Communes Bouzonvillois Trois Frontières et le Département de la
+            Moselle et la Région Grand Est – pour leur soutien fidèle. Leur
+            engagement permet au club de poursuivre son développement, de
+            promouvoir le sport pour tous et de faire rayonner le handball sur
+            notre territoire.
+          </p>
+        </div>
+
         <PartenaireSection title="Fédéraux" partenaires={partenairesFederaux} />
+        <div className="mb-8">
+          <p className="text-foreground/80 mb-12">
+            Le Bouzonville Handball s’inscrit pleinement dans la dynamique du
+            handball français grâce au soutien et à l’accompagnement de ses
+            partenaires fédéraux : la Fédération Française de Handball, la Ligue
+            Grand Est et le Comité Mosellan. Leur appui technique, réglementaire
+            et structurel est essentiel au bon fonctionnement du club, à la
+            formation de nos encadrants, ainsi qu’à la progression de nos
+            joueuses et joueurs dans le respect des valeurs du handball.
+          </p>
+        </div>
+
         <PartenaireSection title="Sponsors" partenaires={sponsors} />
 
         <div className="text-center mb-8">
